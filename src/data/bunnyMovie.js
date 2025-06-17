160ceb2e2b9d9c84f6fb4ed4bcbb645f43509056
Mhalid <musasizihalid@gmail.com>
2025-06-17 21:30:26 +0300

    Fix: add straw

--- src/data/bunnyMovie.js
+++ src/data/bunnyMovie.js
@@ -2,7 +2,7 @@ export const getBunnyVideoUrl = (tmdbId) => {
   const movieMap = {
     603692: "https://your-bunny-cdn.net/videos/avengers.mp4",
     567189: "https://your-bunny-cdn.net/videos/superman.mp4",
-    1426776: "https://curemindset.b-cdn.net/movies/1_5066621091997288050.mkv", // STRAW
+    1426776: "https://curemindset.b-cdn.net/movies/Straw.mp4", // STRAW
     // Add more mappings as you upload
   };
   return movieMap[tmdbId] || null;
